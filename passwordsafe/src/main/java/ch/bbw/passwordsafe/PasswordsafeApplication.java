ffef4d595e7c7cd282d038fb0d48c99a697fdf89
justindavidcalle <justincalle@outlook.de>
2023-11-08 09:18:48 +0100

    Springboot Application gestartet

---
+++ passwordsafe/src/main/java/ch/bbw/passwordsafe/PasswordsafeApplication.java
@@ -0,0 +1,13 @@
+package ch.bbw.passwordsafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordsafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordsafeApplication.class, args);
+	}
+
+}
