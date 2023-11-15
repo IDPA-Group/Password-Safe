3245a6153037d39472dfdb2794da6470b8f33008
david-hofstetter <david.hofstetter@lernende.bbw.ch>
2023-11-15 08:05:48 +0100

    login page

---
+++ passwordsafe-frontend/src/components/loginPage.jsx
@@ -0,0 +1,14 @@
+function LoginPage(){
+    return(
+        <>
+        <h1>Login</h1>
+        <div>
+            <p>Username: <input type="username"/></p>
+            <p>Password: <input type="password"/></p>
+            <button>Sign In</button>
+            
+        </div>
+        </>
+    )
+}
+export default LoginPage
