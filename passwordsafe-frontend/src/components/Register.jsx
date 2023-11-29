a9182c16a1ac5069b3744be730c5dd0cde514ce2
justindavidcalle <justincalle@outlook.de>
2023-11-29 07:54:40 +0100

    Verbessertes Frontend

--- passwordsafe-frontend/src/components/Register.jsx
+++ passwordsafe-frontend/src/components/Register.jsx
@@ -32,6 +32,9 @@ const Register = () => {
       console.error(error);
       setError('Registration failed. Please try again.'); 
     }
+    setMastername('')
+    setMasterPassword('')
+
   };
 
   return (
